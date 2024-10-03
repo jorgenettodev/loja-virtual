b7929dc2ab35fd35dabbafb305788dfcb0058ed2
Jorgenettodev <jorgenetto96@gmail.com>
2024-10-03 20:14:15 -0300

    Cria metodo cadastrarCategoria

--- src/main/java/org/programandoseufuturo/lojavirtual/controller/CategoriaController.java
+++ src/main/java/org/programandoseufuturo/lojavirtual/controller/CategoriaController.java
@@ -1,9 +1,15 @@
 package org.programandoseufuturo.lojavirtual.controller;
 
+import org.programandoseufuturo.lojavirtual.model.Categoria;
 import org.programandoseufuturo.lojavirtual.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +25,14 @@ public class CategoriaController {
         this.service = service;
     }
     
-    @GetMapping
-    public String helloWorld() {
-        return "hello world";
+    @PostMapping
+    public ResponseEntity cadastrarCategoria(@RequestBody Categoria categoria) {
+        Categoria categoriaCriada = service.cadastrarCategoria(categoria);
+
+        if (categoriaCriada != null) {
+            return ResponseEntity.status(HttpStatus.CREATED.value()).body(categoriaCriada);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).build();
     }
 }
